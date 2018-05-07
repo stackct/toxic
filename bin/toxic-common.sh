b5c857c3e01c524aaf062249b66319420dc1b8d1
Ariel Alonso <aalonso@stackct.com>
2018-05-07 12:34:15 -0400

    PTOXIC-18 Use ';' as classpath separator when OS is Windows

--- bin/toxic-common.sh
+++ bin/toxic-common.sh
@@ -9,8 +9,13 @@ if [ -z "${TOXIC_HOME}" ]; then
 fi
 
 classpath=""
+classpathSeparator=":"
 doDir=""
 otherArgs=""
+
+if [ $OSTYPE == 'msys' ]; then
+  classpathSeparator=";"
+fi
 
 while [[ $# != 0 ]]; do
   arg=$1
@@ -28,16 +33,16 @@ while [[ $# != 0 ]]; do
 done
 
 if [ -n "${classpath}" ]; then
-  classpath="${classpath}:"
+  classpath="${classpath}${classpathSeparator}"
 fi
 
-classpath="$classpath/conf"
-classpath="$classpath:${TOXIC_HOME}/conf"
-classpath="$classpath:${TOXIC_HOME}/resources"
-classpath="$classpath:${TOXIC_HOME}/gen/classes"
-classpath="$classpath:${TOXIC_HOME}/gen/build/classes"
-classpath="$classpath:${TOXIC_HOME}/lib/*"
-classpath="$classpath:${TOXIC_HOME}"
+classpath="${classpath}/conf"
+classpath="${classpath}${classpathSeparator}${TOXIC_HOME}/conf"
+classpath="${classpath}${classpathSeparator}${TOXIC_HOME}/resources"
+classpath="${classpath}${classpathSeparator}${TOXIC_HOME}/gen/classes"
+classpath="${classpath}${classpathSeparator}${TOXIC_HOME}/gen/build/classes"
+classpath="${classpath}${classpathSeparator}${TOXIC_HOME}/lib/*"
+classpath="${classpath}${classpathSeparator}${TOXIC_HOME}"
 
 if [ -n "${TOXIC_HEAP_MAX}" ]; then
   TOXIC_JRE_OPTIONS="${TOXIC_JRE_OPTIONS} -Xmx${TOXIC_HEAP_MAX}"
@@ -46,4 +51,4 @@ if [ -n "${TOXIC_PERMGEN_MAX}" ]; then
   TOXIC_JRE_OPTIONS="${TOXIC_JRE_OPTIONS} -XX:MaxPermSize=${TOXIC_PERMGEN_MAX}"
 fi
 
-baseToxicCmd="java -XX:+CMSClassUnloadingEnabled $TOXIC_JRE_OPTIONS -cp $classpath -DTOXIC_HOME=${TOXIC_HOME}"
+baseToxicCmd="java -XX:+CMSClassUnloadingEnabled $TOXIC_JRE_OPTIONS -cp '${classpath}' -DTOXIC_HOME=${TOXIC_HOME}"
