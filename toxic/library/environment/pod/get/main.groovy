70db4cfcb71e69829312a5f346d1f12d4046f0b1
Adam Kohring <akohring@stackct.com>
2018-05-18 14:11:43 -0400

    PTOXIC-30 Updated getPod lib to set the name based on the contents of the out variable.

--- toxic/library/environment/pod/get/main.groovy
+++ toxic/library/environment/pod/get/main.groovy
@@ -1,1 +1,2 @@
-memory.name = execWithEnv(['/bin/sh', '-c', "kubectl -n ${memory['namespace']} get pods | grep '^${memory['prefix']}' | awk '{print \$1}'"])
+execWithEnv(['/bin/sh', '-c', "kubectl -n ${memory['namespace']} get pods | grep '^${memory['prefix']}' | awk '{print \$1}'"])
+memory.name = out.toString().trim()
