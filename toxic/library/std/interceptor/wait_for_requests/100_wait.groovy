ae9ea9074dec98086c0912504ac7d603ce44453d
Jason Ertel <jertel@stackct.com>
2019-05-07 01:55:08 -0400

    PTOXIC-135 output success to give a more clear indicator that the wait succeeded

--- toxic/library/std/interceptor/wait_for_requests/100_wait.groovy
+++ toxic/library/std/interceptor/wait_for_requests/100_wait.groovy
@@ -1,6 +1,6 @@
 import util.Wait
 
 Wait.on { -> 
-    ok = memory.requestCounter >= memory.waitForCount
-    return ok
+    memory.success = memory.requestCounter >= memory.waitForCount
+    return memory.success
 }.every(1000).atMostMs(memory.waitForAtMostMilliseconds).start()
