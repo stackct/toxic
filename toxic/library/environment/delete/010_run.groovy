cfe0b3eb2012fbe0cb3515462ba77a7b5313c517
Jason Ertel <jertel@stackct.com>
2019-05-12 12:07:11 -0400

    PTOXIC-137 parallelize helm delete and install

--- toxic/library/environment/delete/010_run.groovy
+++ toxic/library/environment/delete/010_run.groovy
@@ -14,3 +14,8 @@ int wait = 1000 * 600
 int interval = 1000 * 3
 
 Wait.on { -> !memory.namespaceExists() }.every(interval).atMostMs(wait).start()
+
+// This is ugly but since Kubernetes lies about the namespace being deleted when it's 
+// still being terminated we have no choice to but wait 5 seconds and hope it's actuall
+// completely gone.
+sleep(5000)
