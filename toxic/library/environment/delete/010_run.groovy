1fe5aa58c10836b47e1ae1d12b197edfc501ca53
Jason Ertel <jertel@stackct.com>
2019-05-16 12:14:39 -0400

    PTOXIC-137 re-enabled parallel deletes since the namespace problem was actually kubectl returning a 1 (failed to list namespaces) and pickle incorrectly assumed the namespace was finished being deleted

--- toxic/library/environment/delete/010_run.groovy
+++ toxic/library/environment/delete/010_run.groovy
@@ -1,7 +1,7 @@
 import util.Wait
 
 // Delete the charts
-memory.parseEnvironment(memory.spec).charts.each { chart, props ->
+memory.inParallelMap(memory.parseEnvironment(memory.spec).charts) { chart, props ->
   memory.helmDelete(chart)
 }
 
@@ -10,7 +10,13 @@ memory.deleteNamespace()
 
 
 // Wait for namespace to be deleted
-int wait = 1000 * 600
+int wait = 1000 * 200
 int interval = 1000 * 3
+int verifications = 3
 
-Wait.on { -> !memory.namespaceExists() }.every(interval).atMostMs(wait).start()
+// Occassionally kubectl returns a failure when querying namespaces. Check multiple
+// times for the namespace before concluding it truly does not exist.
+verifications.times {
+  Wait.on { -> !memory.namespaceExists() }.every(interval).atMostMs(wait).start()
+  sleep(interval)
+}
