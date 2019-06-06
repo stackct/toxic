37ecf69c61461fcd27b038a9e80cca1394bc96f8
Jason Ertel <jertel@stackct.com>
2019-06-06 12:14:41 -0400

    PTOXIC-141 support multiple pickle runs

--- src/toxic/TaskMaster.groovy
+++ src/toxic/TaskMaster.groovy
@@ -114,7 +114,14 @@ public class TaskMaster implements Callable {
       memory.taskId++
     }
 
-    TestCaseRunner.run(this, memory, results)
+    int runs = memory['pickle.testCaseRuns'] ? new Integer(memory['pickle.testCaseRuns']) : 1
+    for (memory.pickleRun = 0; memory.pickleRun < runs; memory.pickleRun++) {
+      TestCaseRunner.run(this, memory, results)
+      if (TaskResult.shouldAbort(props, results)) {
+        log.info("Aborting TestCaseRunner due to task failure")
+        break
+      }
+    }
 
     memory.clear()
   }
