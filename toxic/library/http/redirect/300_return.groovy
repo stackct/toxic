e356be897ca560fa9dd2d47078cea3ec6a551ae6
Adam Kohring <akohring@stackct.com>
2019-08-01 13:04:05 -0400

    PTOXIC-150 Check for multiple casings of the location header when setting the redirectUrl

--- toxic/library/http/redirect/300_return.groovy
+++ toxic/library/http/redirect/300_return.groovy
@@ -1,3 +1,3 @@
-memory.redirectUrl = memory['http.response.headers']['Location']
+memory.redirectUrl = memory['http.response.headers']['Location'] ?: memory['http.response.headers']['location']
 memory.redirectHttpUri = memory['http.response.location']["httpUri"]
 memory.redirectHttpMethod = memory['http.response.location']['httpMethod']
